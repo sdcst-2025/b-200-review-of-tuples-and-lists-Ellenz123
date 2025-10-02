75b360d682579c9b8a2eef59eadef9eebf5fc955
Ellenz123 <ellenz759@deltalearns.ca>
2025-10-02 10:01:14 -0700

    I am done.

--- 200a-Review.py
+++ 200a-Review.py
@@ -5,7 +5,10 @@ def getIntegers(myList):
     # myList : expected list or tuple
     # iterate through myList and add all the integers to the new list
     integers = []
-
+    for i in myList:
+        if i==int(i):
+            integers.append(i)
+    print(integers)
     return integers
 
 def getFactor(myList,number):
@@ -14,14 +17,22 @@ def getFactor(myList,number):
     # iterate through the list and add the number to the list if
     # it is a factor of the number
     factors = []
-
+    for i in myList:
+        if i==int(i) and i !=0:
+            if number % i ==0:
+                factors.append(i)
+    print(factors)
     return factors
 
 def getNegatives(myList):
     # myList : expected list or tuple
     # iterate through myList and add all the negative numbers to the new list
     negatives = []
-
+    for i in myList:
+        if i==int(i) or i==float(i):
+            if i<0:
+                negatives.append(i)
+    print(negatives)
     return negatives
 
 def getIntersection(list1,list2):
@@ -30,7 +41,10 @@ def getIntersection(list1,list2):
     # return a sorted list of numbers that is in both lists
     # the intersection of the 2 number sets
     common = []
-
+    for i in list1:
+        if i in list2:
+            common.append(i)
+    print(common)
     return common
 
 def getUnion(list1,list2):
@@ -39,7 +53,11 @@ def getUnion(list1,list2):
     # return a sorted list of numbers that is in either of the lists
     # duplicate values will be ignored
     union = []
-
+    for i in list1 + list2:
+        if i not in union:
+            union.append(i)
+        union.sort()
+    print(union)
     return union   
 
 def getMerge(list1,list2):
@@ -48,7 +66,13 @@ def getMerge(list1,list2):
     # add the elements of list2 into list1
     # if the list2 element is in list1, add it at the position where it occurs in list1
     # if the list2 element is not in list1, add it to the end
-
+    for i in list2:
+        if i in list1:
+            index=list1.index(i)
+            list1.insert(index+1, i)
+        else:
+            list1.append(i)
+    print(list1)
     return list1
 
 
